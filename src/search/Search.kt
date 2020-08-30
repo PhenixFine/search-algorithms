b5cc80340f535fcb797c5708e49e4147834e5ca0
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-08-30 17:42:47 -0400

    Search.kt -  adjusted the var totalTries to Long to accept larger numbers ( because of the LinearSearch class I added, I didn't get to add notes to it because I accidently included it with BinarySearch's update, but it just does a linear search, which I wouldn't recommend doing for large arrays ). • val time ( of new class TrackTime() ) was added to track the time each search takes ( got the idea from a project I'm working on for Hyperskill, which is similar to this project, and will also have sort algorithms ). • adjusted the lambda val printTries to format the numbers properly. • find() - adjusted to reset the search class properties. Added the tracking of each search algorithms time it takes to find a field. • reset() - adjusted from protected to private to reduce confusion on what is being reset for the child classes. • average() - was adjusted to also show the time it took to find the average steps.

--- src/search/Search.kt
+++ src/search/Search.kt
@@ -1,29 +1,37 @@
+package search
+
+import utility.TrackTime
+import utility.numberFormat
+
 abstract class Search(protected val numbers: Array<Int>) {
     private var tries = 0
-    private var totalTries = 0
+    private var totalTries = 0L
     private var numOfTries = 0
     protected var find = 0
     protected var returnNum: Int? = null
     protected var printStep = true
     protected val lastIndex = if (numbers.isEmpty()) -1 else numbers.lastIndex
+    private val time = TrackTime()
     protected val printTries = { steps: Int ->
         tries++
-        if (printStep) println("Step $tries: Checking index $steps, with a value of ${numbers[steps]}.")
+        if (printStep) println("Step $tries: Checking index $steps, with a value of ${numberFormat(numbers[steps])}.")
     }
 
     fun find(number: Int, printSteps: Boolean = true): Int {
         if (numbers.isEmpty()) return -1
+        reset()
         if (printStep != printSteps) printStep = printSteps
         find = number
-
+        time.start()
         findWork()
+        time.stop()
 
         return returnNum ?: -1
     }
 
     protected abstract fun findWork()
 
-    protected open fun reset() {
+    private fun reset() {
         if (returnNum != null) returnNum = null
         resetTries()
     }
@@ -38,7 +46,11 @@ abstract class Search(protected val numbers: Array<Int>) {
 
     open fun average() {
         resetTries()
-        val average = if (numOfTries == 0) 0 else totalTries / numOfTries
-        println("${this.javaClass.name.replace("S", " s")} average for $numOfTries searches is $average steps.")
+        val name = this.javaClass.name.replace("S", " s").removeRange(0..6)
+        val average = if (numOfTries == 0) 0 else (totalTries / numOfTries).toInt()
+        println(
+            "$name average for ${numberFormat(numOfTries)} searches " +
+                    "is ${numberFormat(average)} steps. Time taken: ${time.elapsed()}"
+        )
     }
 }
