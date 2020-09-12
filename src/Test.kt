24de6311cef0021c2d4f8dc7e5bb3345feefd80c
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-09-12 14:44:16 -0400

    Test.kt - updated the following: • some standard functions were moved to the package utility, so the package is now imported • val PATH was added so that the data could be stored in the folder data • removed the variable IMPORTED • added the variable DO_LIN_SEARCH for adding linear search to the averages along with Binary and Jump ( I wouldn't recommend using it for large arrays, and the option is disabled by default ). • main() -  for option 4 and 6, the function calls was replaced with updateNumbers(), so that Binary and Jump are also updated with the new array for doing a search. • menu() - was updated to add the option of finding average search time for a Linear search in addition to Binary and Jump. • search() - was updated to format numbers. • average() - the optional linear search was added. • newArray() - was removed and replaced with updateNumbers() for multi use. • updateNumbers() - receives an array that it then assigns to NUMBERS, JUMP_SEARCH, and BINARY_SEARCH. • export() - it takes way too long to store a really large array the way I had it, so I switched to using object serialization ( the files are larger, but it's much faster for exporting ). Time tracking was also added. • import() - was adjusted for object serialization. It takes longer than before ( I think before it took just a few hundred milliseconds for really larges files, where now it takes a few seconds, but the old export() was taking 20 to 30 minutes for really large arrays, so it's definitely worth the sacrifice ). Time tracking was also added. • importConfirm() - was removed. I just have import() confirm the starting and succeeding of import now. • options() - added an option to add linear search to the average time it takes to search the array. • getArray() - it was taking way too long to create a large array so I adjusted it where it'll get a random number within a range that increases according to the rangeSize divided by the arraySize. This also prevents having to sort the array when it's done. Time tracking was also added. • optionMenu() - was adjusted for the linear search in average options, and I added number formatting to the MIN and MAX. • getMin() and getMax() was adjusted to format the numbers. • getFileName(), IntRange.random(), getNum(), getString(), and isNumber() was moved to the file Utilities.kt in the utility package.

--- src/Test.kt
+++ src/Test.kt
@@ -1,24 +1,23 @@
 /** When adding a new algorithm update:
- *      main(), menu(), average(), and newArray().
+ *      main(), menu(), average(), and updateNumbers().
  *  If there are any new options for it update:
  *      options() and optionMenu() */
 
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.IOException
-import java.util.Random
+import search.*
+import utility.*
+import java.io.*
 import kotlin.math.absoluteValue
 
 private var MIN = 0
 private var MAX = 400_000
-private var NUMBERS = import("numbers50K.txt")
+private const val PATH = "data/"
+private var NUMBERS = import("numbers50K.dat")
 private var JUMP_SEARCH = JumpSearch(NUMBERS)
 private var BINARY_SEARCH = BinarySearch(NUMBERS)
 private var PRINT_SEARCH = true
 private var PRINT_ARRAY = false
 private var LIN_FORWARD = true
-private var IMPORTED = true
-
+private var DO_LIN_SEARCH = false
 
 fun main() {
     var command = getNum(menu())
@@ -28,9 +27,9 @@ fun main() {
             1 -> search(BINARY_SEARCH)
             2 -> search(JUMP_SEARCH)
             3 -> average()
-            4 -> newArray()
+            4 -> updateNumbers(getArray())
             5 -> export()
-            6 -> importConfirm()
+            6 -> updateNumbers(import(getFileName()))
             7 -> options()
         }
         command = getNum(menu())
@@ -38,9 +37,11 @@ fun main() {
 }
 
 private fun menu(): String {
+    val strLinear = { if (DO_LIN_SEARCH) ", Jump, and Linear" else " and Jump" }
+
     return ("\n1 - Binary Search\n" +
             "2 - Jump Search\n" +
-            "3 - Get Average Search steps for Binary versus Jump for the Array\n" +
+            "3 - Get Average steps for Binary" + strLinear() + " searches of the Array\n" +
             "4 - Create a new random Array\n" +
             "5 - export an Array of Numbers\n" +
             "6 - import an Array of numbers\n" +
@@ -57,7 +58,10 @@ private fun search(searchObject: Search) {
         val find = getNum("What number would you like to find the Index of?")
         val found = searchObject.find(find, PRINT_SEARCH)
 
-        println(if (found == -1) "The number is not in the array." else "Found $find at index number: $found")
+        println(
+            if (found == -1) "The number is not in the array." else "Found ${numberFormat(find)} at index number: " +
+                    numberFormat(found)
+        )
     }
 
     searchObject.average()
@@ -66,30 +70,36 @@ private fun search(searchObject: Search) {
 private fun average() {
     val jumpSearch = JumpSearch(NUMBERS)
     val binarySearch = BinarySearch(NUMBERS)
+    val linearSearch = LinearSearch(NUMBERS)
 
+    println("Finding Averages...")
     NUMBERS.forEach {
         jumpSearch.find(it, false)
         binarySearch.find(it, false)
+        if (DO_LIN_SEARCH) linearSearch.find(it, false)
     }
 
     binarySearch.average()
     jumpSearch.average()
+    if (DO_LIN_SEARCH) linearSearch.average()
 }
 
-private fun newArray() {
-    NUMBERS = getArray()
-    BINARY_SEARCH = BinarySearch(NUMBERS)
+private fun updateNumbers(newArray: Array<Int>) {
+    NUMBERS = newArray
     JUMP_SEARCH = JumpSearch(NUMBERS)
+    BINARY_SEARCH = BinarySearch(NUMBERS)
 }
 
 private fun export() {
     try {
-        val file = File(getFileName())
-        var numbers = ""
+        val file = File(PATH + getFileName())
+        val time = TrackTime()
 
-        for (num in NUMBERS) numbers += "$num "
-        file.writeText(numbers.trim())
-        println("The array of numbers have been saved to the file ${file.name}")
+        println("Exporting Array to File ${file.name}...")
+        time.start()
+        ObjectOutputStream(FileOutputStream(file)).use { it.writeObject(NUMBERS) }
+        time.stop()
+        println("The array of numbers have been saved to the file ${file.name}. Time taken: ${time.elapsed()}")
     } catch (e: IOException) {
         println(
             "Access was denied in writing to the file. Please ensure it is not set to read only, or open in " +
@@ -101,11 +111,18 @@ private fun export() {
 }
 
 private fun import(fileName: String? = null): Array<Int> {
-    if (!IMPORTED) IMPORTED = true
     try {
-        val file = File(fileName ?: getFileName())
+        val file = File(PATH + (fileName ?: getFileName()))
+        val numbers: Array<Int>
+        val time = TrackTime()
 
-        return (file.readText().split(" ").map { it.toInt() }).toTypedArray()
+        println("Importing File ${file.name} to an array...")
+        time.start()
+        ObjectInputStream(FileInputStream(file)).use { numbers = it.readObject() as? Array<Int> ?: throw Exception() }
+        time.stop()
+        println("The file ${file.name} has been imported. Time taken: ${time.elapsed()}")
+
+        return numbers
     } catch (e: FileNotFoundException) {
         println("File not found.\n")
     } catch (e: Exception) {
@@ -114,8 +131,7 @@ private fun import(fileName: String? = null): Array<Int> {
                     "formatted correctly.\n"
         )
     }
-    IMPORTED = false
-    return if (NUMBERS.isNotEmpty()) {
+    return if (!NUMBERS.isNullOrEmpty()) {
         NUMBERS
     } else {
         println("Import failed, loading a random array of size 100.")
@@ -123,21 +139,17 @@ private fun import(fileName: String? = null): Array<Int> {
     }
 }
 
-private fun importConfirm(fileName: String = getFileName()) {
-    NUMBERS = import(fileName)
-    if (IMPORTED) println("Loaded $fileName")
-}
-
 private fun options() {
     var command = getNum(optionMenu())
 
-    while (command != 6) {
+    while (command != 7) {
         when (command) {
             1 -> PRINT_SEARCH = !PRINT_SEARCH
             2 -> PRINT_ARRAY = !PRINT_ARRAY
             3 -> jumpLinChange()
-            4 -> getMin()
-            5 -> getMax()
+            4 -> DO_LIN_SEARCH = !DO_LIN_SEARCH
+            5 -> getMin()
+            6 -> getMax()
         }
         command = getNum(optionMenu())
     }
@@ -146,34 +158,44 @@ private fun options() {
 private fun getArray(size: Int? = null): Array<Int> {
     val numbers = mutableListOf<Int>()
     val arraySize = size ?: getNum("Enter Array size:")
-    val range = MIN..MAX
+    val rangeSize = MAX - MIN + 1
+    val increment = if (arraySize <= 0) 0 else rangeSize / arraySize
+    var min = MIN
+    var max = min + increment - 1
+    val range = { min..max }
+    val time = TrackTime()
 
-    if (arraySize > ((MAX - MIN + 1) / 4) || arraySize < 5) {
+    if (arraySize > rangeSize || arraySize < 5) {
         println("Size is too " + if (arraySize < 5) "small" else "large.")
-        println("Please enter an array no smaller than 5 and no larger than ${(MAX - MIN + 1) / 4}.")
+        println("Please enter an array no smaller than 5 and no larger than ${numberFormat(rangeSize)}.")
         return getArray()
     }
-
+    println("Creating Array...")
+    time.start()
     repeat(arraySize) {
-        var add = range.random()
-        while (numbers.contains(add)) add = range.random()
-        numbers.add(add)
+        numbers.add(range().random())
+        min = max + 1
+        max += increment
     }
-    numbers.sort()
+    time.stop()
+    println("Array of size ${numberFormat(arraySize)} was created. Time taken: ${time.elapsed()}")
     return numbers.toTypedArray()
 }
 
 private fun optionMenu(): String {
     val strYesNo = { check: Boolean -> if (check) "YES" else "NO" }
+    val strAddRemove = { if (DO_LIN_SEARCH) "Remove" else "Add" }
+    val strFromTo = { if (DO_LIN_SEARCH) "from" else "to" }
 
     return ("\n1. Print Search Steps: ${strYesNo(PRINT_SEARCH)}\n" +
             "2. Print the numbers in the array before doing a Search: ${strYesNo(PRINT_ARRAY)}\n" +
             "3. Jump Search with: Linear " + (if (LIN_FORWARD) "Forward\n" else "Backwards\n") +
-            "4. Change the minimum range for Random Number\n" +
-            "       Current minimum: $MIN\n" +
-            "5. Change the max range for Random Number\n" +
-            "       Current max: $MAX\n" +
-            "6. Return to Main Menu")
+            "4. " + strAddRemove() + " Linear Search " + strFromTo() + " Average Steps in Main Menu\n" +
+            "5. Change the minimum range for Random Number\n" +
+            "       Current minimum: ${numberFormat(MIN)}\n" +
+            "6. Change the max range for Random Number\n" +
+            "       Current max: ${numberFormat(MAX)}\n" +
+            "7. Return to Main Menu")
 }
 
 private fun jumpLinChange() {
@@ -182,9 +204,10 @@ private fun jumpLinChange() {
 }
 
 private fun getMin() {
-    val minValue = if (MAX < 0 ) Int.MIN_VALUE + 40 else Int.MIN_VALUE + MAX + 40
+    val minValue = if (MAX < 0) Int.MIN_VALUE + 40 else Int.MIN_VALUE + MAX + 40
     var newMin = getNum("Enter new minimum Number:")
-    val strTryAgain = "Number can be no larger than ${MAX - 40}, and no smaller than $minValue. Please try again:"
+    val strTryAgain = "Number can be no larger than ${numberFormat(MAX - 40)}, and no smaller " +
+            "than ${numberFormat(minValue)}. Please try again:"
 
     while (newMin > MAX - 40 || newMin < minValue) newMin = getNum(strTryAgain)
     MIN = newMin
@@ -193,33 +216,9 @@ private fun getMin() {
 private fun getMax() {
     val maxValue = if (MIN < 0) Int.MAX_VALUE - MIN.absoluteValue - 40 else Int.MAX_VALUE - 40
     var newMax = getNum("Enter new max number:")
-    val strTryAgain = "Number can be no smaller than ${MIN + 40}, and no larger than $maxValue. Please try again:"
+    val strTryAgain = "Number can be no smaller than ${numberFormat(MIN + 40)}, " +
+            "and no larger than ${numberFormat(maxValue)}. Please try again:"
 
     while (newMax < MIN + 40 || newMax > maxValue) newMax = getNum(strTryAgain)
     MAX = newMax
 }
-
-private fun getFileName() = getString("File name:")
-
-// this useful random function was found on Stack Overflow
-private fun IntRange.random() = Random().nextInt(endInclusive + 1 - start) + start
-
-private fun getNum(text: String, defaultMessage: Boolean = false): Int {
-    val strErrorNum = " was not a number, please try again: "
-    var num = text
-    var default = defaultMessage
-
-    do {
-        num = getString(if (default) num + strErrorNum else num)
-        if (!default) default = true
-    } while (!isNumber(num))
-
-    return num.toInt()
-}
-
-private fun getString(text: String): String {
-    println(text)
-    return readLine()!!
-}
-
-private fun isNumber(number: String) = number.toIntOrNull() != null
